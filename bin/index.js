0e2fbc74126538adb363daf7bfea6a777ae49b67
truth1984 <zhengyao365@163.com>
2020-05-20 16:55:05 +0800

    add prescript

--- bin/index.js
+++ bin/index.js
@@ -337,6 +337,16 @@ new ucmd("helper")
         yum: "epel-release ",
         common: "sudo psmisc net-tools nethogs openssh-server openssh-clients cronie curl",
         optional: "docker",
+        prescript: `if ! [ -f "$HOME/.bash_mine" ]; then
+        touch $HOME/.bash_mine
+        mkdir $HOME/.npm_global
+        echo 'source $HOME/.bash_mine' >> $HOME/.bashrc
+        echo 'if [ "$PWD" = "$HOME" ]; then cd Documents; fi;' >> $HOME/.bash_mine
+        echo 'PATH=$HOME/.npm_global/bin/:$PATH' >> $HOME/.bash_mine
+        npm config set prefix $HOME/.npm_global
+    
+        git config --global alias.adog "log --all --decorate --oneline --graph"
+    fi`,
       });
     let list = {
       screen: {
