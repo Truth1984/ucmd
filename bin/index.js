2d1f0120a383a9607981039288248a139027c34e
awada <zhengyao365@163.com>
2020-06-03 15:32:47 +0800

    add cdd

--- bin/index.js
+++ bin/index.js
@@ -421,6 +421,7 @@ new ucmd("helper")
         mkdir $HOME/.application
         echo 'source $HOME/.bash_mine' >> $HOME/.bashrc
         echo 'if [ "$PWD" = "$HOME" ]; then cd Documents; fi;' >> $HOME/.bash_mine
+        echo 'function cdd { cd $1 && ls -a; }' >> $HOME/.bash_mine
         echo 'PATH=$HOME/.npm_global/bin/:$PATH' >> $HOME/.bash_mine
         npm config set prefix $HOME/.npm_global
         source $HOME/.bashrc
